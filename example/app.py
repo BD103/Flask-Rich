42251349858b9ea7d0937a6f85f1bdd61b79c930
BD103 <59022059+BD103@users.noreply.github.com>
2021-11-16 17:08:46 +0000

    chore: don't use init_app in example

--- example/app.py
+++ example/app.py
@@ -1,12 +1,10 @@
 from flask import Flask
+
 from flask_rich import RichApplication
 
-
-rich = RichApplication()
-
 app = Flask(__name__)
+rich = RichApplication(app)
 
-rich.init_app(app)
 
 app.logger.debug("If this is [red]colorful[/red], everything worked!")
 app.logger.debug("1, 2.4, True, False, None")
