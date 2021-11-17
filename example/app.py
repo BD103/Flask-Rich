948735193139164c90798d4ba744e102585a195a
BD103 <59022059+BD103@users.noreply.github.com>
2021-11-17 17:49:32 +0000

    feat: add rich-routes command

--- example/app.py
+++ example/app.py
@@ -8,3 +8,13 @@ rich = RichApplication(app)
 
 app.logger.debug("If this is [red]colorful[/red], everything worked!")
 app.logger.debug("1, 2.4, True, False, None")
+
+
+@app.route("/")
+def index():
+    return "INDEX"
+
+
+@app.route("/test/<string:x>", methods=["GET", "POST"])
+def test(x: str):
+    return x
--- src/flask_rich/core.py
+++ src/flask_rich/core.py
@@ -1,12 +1,17 @@
 import logging
+from operator import attrgetter
 
+import click
+from rich.console import Console
 from rich.logging import RichHandler
+from rich.table import Column, Table
 from rich.traceback import install as install_traceback
 
 
 class RichApplication(object):
     def __init__(self, app=None):
         self.app = app
+        self.console = Console()
 
         if app is not None:
             self.init_app(app)
@@ -18,6 +23,8 @@ class RichApplication(object):
             ("RICH_TRACEBACK", True),
             ("RICH_TRACEBACK_EXTRA_LINES", 1),
             ("RICH_TRACEBACK_SHOW_LOCALS", False),
+            ("RICH_ROUTES", True),
+            ("RICH_ROUTES_MODE", "table"),
         ]
 
         for k, v in defaults:
@@ -36,3 +43,38 @@ class RichApplication(object):
                 extra_lines=app.config["RICH_TRACEBACK_EXTRA_LINES"],
                 show_locals=app.config["RICH_TRACEBACK_SHOW_LOCALS"],
             )
+
+        if app.config["RICH_ROUTES"]:
+            if app.config["RICH_ROUTES_MODE"] == "table":
+
+                @app.cli.command(
+                    "rich-routes", short_help="Show the routes for the app."
+                )
+                @click.option(
+                    "--all-methods", is_flag=True, help="Show HEAD and OPTIONS methods."
+                )
+                def rich_routes_command(all_methods: bool) -> None:
+                    rules = list(app.url_map.iter_rules())
+
+                    if not rules:
+                        click.echo("No routes were registered.")
+                        return
+
+                    ignored_methods = set(() if all_methods else ("HEAD", "OPTIONS"))
+
+                    rule_methods = [
+                        ", ".join(sorted(rule.methods - ignored_methods))  # type: ignore
+                        for rule in rules
+                    ]
+
+                    table = Table(
+                        Column("Endpoint", style="bright_magenta"),
+                        Column("Methods", style="cyan"),
+                        Column("Rule", style="green"),
+                        title="App Routes",
+                    )
+
+                    for rule, methods in zip(rules, rule_methods):
+                        table.add_row(rule.endpoint, methods, rule.rule)
+
+                    self.console.print(table)
